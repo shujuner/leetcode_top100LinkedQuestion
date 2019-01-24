bc5f7d31cdc0fc3395651aaaf9350c85f4fdf151
shujuner <2093317940@qq.com>
2019-01-24 23:01:23 +0800

    map and unordered_map

---
+++ 1_twoSum/cppVersion.cpp
@@ -0,0 +1,88 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int> &numbers, int target)
+    {
+        //Key is the number and value is its index in the vector.
+        unordered_map<int, int> hash;
+        vector<int> result;
+        int length=numbers.size();
+        for (int i = 0; i < length; i++) {
+            int numberToFind = target - numbers[i];
+
+                //if numberToFind is found in map, return them
+            if (hash.find(numberToFind) != hash.end()) {
+                        //+1 because indices are NOT zero based
+                result.push_back(hash[numberToFind]);
+                result.push_back(i);			
+                return result;
+            }
+
+                //number was not found. Put it in the map.
+            hash[numbers[i]] = i;
+        }
+        return result;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+int stringToInteger(string input) {
+    return stoi(input);
+}
+
+string integerVectorToString(vector<int> list, int length = -1) {
+    if (length == -1) {
+        length = list.size();
+    }
+
+    if (length == 0) {
+        return "[]";
+    }
+
+    string result;
+    for(int index = 0; index < length; index++) {
+        int number = list[index];
+        result += to_string(number) + ", ";
+    }
+    return "[" + result.substr(0, result.length() - 2) + "]";
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        vector<int> nums = stringToIntegerVector(line);
+        getline(cin, line);
+        int target = stringToInteger(line);
+        
+        vector<int> ret = Solution().twoSum(nums, target);
+
+        string out = integerVectorToString(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
